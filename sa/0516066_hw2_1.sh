de344c7070f8868fd62ab49191fdb07b9bd1113f
tengwc10230 <44221209+tengwc10230@users.noreply.github.com>
2018-10-30 00:22:55 +0800

    delete

--- sa/0516066_hw2_1.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-ls -ARl | sort -k5nr | grep '^-\|^d' | awk '{if ($1~/^-/){sum += $5; file++; if(file <= 5 ) print file":"$5,$9;}}END{print "Dir num: "NR-file"\n" "File num: "file "\n" "Total: "sum}'
